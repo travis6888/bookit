7945b5b1207d6e977ba8e7a3f637269134c730ec
Pavan <ppatel890@gmail.com>
2014-08-11 18:00:43 -0700

    oauth

--- bookit/settings.py
+++ bookit/settings.py
@@ -26,8 +26,17 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTHENTICATION_BACKENDS = (
+    'social.backends.google.GoogleOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '360250540164-jqs8ikfu35rt2341got6vffh4v19vvfb.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'Tr9XHh133KFmQncapPieMpIq'
+
+# SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [...]
+
+LOGIN_REDIRECT_URL = 'home'
 # Application definition
 
 INSTALLED_APPS = (
