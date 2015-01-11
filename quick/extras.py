30388a8dc49f6b50e973ca845b15bdb23836289b
travis6888 <travis6888@yahoo.com>
2015-01-10 18:36:47 -0800

    starting to fix

---
+++ quick/extras.py
@@ -0,0 +1,5 @@
+__author__ = 'travis6888'
+# calendar3 = service.freebusy().query(body=freebusy_query)
+    # print calendar3['freebusy_query']
+    # for i in range(len(calendar3['body'])-1):
+    #     print i
