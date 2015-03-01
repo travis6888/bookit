679de6f67e7892dceac6326176eca8a586eb167d
travis6888 <travis6888@yahoo.com>
2015-02-28 22:16:45 -0800

    delete old freetimes for all

--- quick/views.py
+++ quick/views.py
@@ -1,4 +1,3 @@
-
 # Date imports
 import datetime
 from dateutil import tz
@@ -28,7 +27,7 @@ import httplib2
 from oauth2client.client import AccessTokenCredentials
 from requests import get
 
-#Other imports
+# Other imports
 import json
 import sendgrid
 from pyzipcode import ZipCodeDatabase
@@ -51,7 +50,7 @@ def profile(request):
 
     # # Loops through events and determines the time your calendar event ends, and how much time you have until your
     # # next one
-    for i in range(len(calendar2['items'])-1):
+    for i in range(len(calendar2['items']) - 1):
         next_start = calendar2['items'][i + 1]['start']['dateTime']
         current_end = calendar2['items'][i]['end']['dateTime']
         event = (str(calendar2['items'][i]['summary']))
@@ -77,7 +76,7 @@ def profile(request):
                     for j in range(difference.days):
                         if j == 0:
                             free_start_dateTime = curent_event_end_dateTime
-                            free_end_dateTime = free_start_dateTime+relativedelta(hours=7)
+                            free_end_dateTime = free_start_dateTime + relativedelta(hours=7)
                             free_time_start = free_start_dateTime.strftime('%Y-%m-%dT%H:%M:%S-08:00')
                             free_time_end = free_end_dateTime.strftime('%Y-%m-%dT%H:%M:%S-08:00')
                             free_time_amount = free_end_dateTime - free_start_dateTime
@@ -127,12 +126,12 @@ def create_profile(request):
     # Pulls information about the user from database to save
     user_social_auth = request.user.social_auth.filter(provider='google-oauth2').first()
     # access_token = user_social_auth.extra_data['access_token']
-    calID=user_social_auth.uid
+    calID = user_social_auth.uid
     if request.method == "POST":
         form = ProfileCreationForm(request.POST)
         if form.is_valid():
             profile = form.save(commit=False)
-            profile.email =calID
+            profile.email = calID
             # profile.oauth_token = access_token
             profile.user = request.user
             profile.save()
@@ -144,9 +143,10 @@ def create_profile(request):
     return render(request, 'create_profile.html', data)
 
 
-
 """Pulls events from eventbrite Api using information from user profile. Will search for events that match the open
 times and the interests of the user"""
+
+
 @csrf_exempt
 def eventbrite_api(request):
     profile = Profile.objects.get(user=request.user)
@@ -156,31 +156,31 @@ def eventbrite_api(request):
     #geocodes users zipcode to use lat and lng in search
     zipcode = zcdb[profile.zipcode]
     free_times = FreeTimes.objects.filter(user=request.user)
-    eventbrite_url='https://www.eventbriteapi.com/v3/events/search/?'
+    eventbrite_url = 'https://www.eventbriteapi.com/v3/events/search/?'
 
     # for free_time in free_times:
 
     # formats time to match eventbrite api
-    start_time =  "{}Z".format(free_times.first().free_time_start[:-6])
+    start_time = "{}Z".format(free_times.first().free_time_start[:-6])
     end_time = "{}Z".format(free_times.last().free_time_end[:-6])
     print start_time, end_time
     for interest in interests:
         eventbrite_params = {
-        "token": eventbrite_token,
-        'popular': True,
-        'q': str(interest.interests),
-        'location.latitude': zipcode.latitude,
-        'location.longitude': zipcode.longitude,
-        'location.within': '40mi',
-        'start_date.range_start': start_time,
-        'start_date.range_end': end_time
+            "token": eventbrite_token,
+            'popular': True,
+            'q': str(interest.interests),
+            'location.latitude': zipcode.latitude,
+            'location.longitude': zipcode.longitude,
+            'location.within': '40mi',
+            'start_date.range_start': start_time,
+            'start_date.range_end': end_time
         }
         eventbrite_resp = get(url=eventbrite_url, params=eventbrite_params)
         eventbrite_data = json.loads(eventbrite_resp.text)
 
         # Saves returned events to database
         for event in eventbrite_data['events']:
-            formatted_start = event['start']['local']+ str('.000-08:00')
+            formatted_start = event['start']['local'] + str('.000-08:00')
 
             # print formatted_start.astimezone(est)
             formatted_end = str(event['end']['local']) + str('.000-08:00')
@@ -191,7 +191,6 @@ def eventbrite_api(request):
             # Creates a datetime object from the time returned by Api
 
             datetime_start = dateutil.parser.parse(event['start']['utc'])
-
 
             datetime_end = dateutil.parser.parse(event['end']['utc'])
 
@@ -232,16 +231,17 @@ def eventbrite_api(request):
     return HttpResponse(json.dumps(success), content_type="application/json")
 
 
-
 """Pulls events from meetup Api using information from user profile. Will search for events that match the open
 times and the interests of the user"""
+
+
 @csrf_exempt
 def meetup_api(request):
     profile = Profile.objects.get(user=request.user)
     interests = profile.interests.filter(interests__in=['MUSIC', 'TECHNOLOGY', 'COMEDY', 'CAR', 'FOOD', 'SPORTS'])
     zcdb = ZipCodeDatabase()
     zipcode = zcdb[profile.zipcode]
-    tz=get_localzone()
+    tz = get_localzone()
 
     # Maps profile interests to Meetup API's category keys
     meetup_category = {
@@ -253,15 +253,15 @@ def meetup_api(request):
         'SPORTS': 9
     }
     free_times = FreeTimes.objects.filter(user=request.user)
-    meetup_url= 'https://api.meetup.com/2/open_events.json?'
+    meetup_url = 'https://api.meetup.com/2/open_events.json?'
     start_time = free_times.first().free_time_start
     end_time = free_times.last().free_time_end
     meetup_start = datetime.datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S-08:00')
     meetup_end = datetime.datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%S-08:00')
 
     # Converts free times to unix time for Meetup Api
-    meetup_epoch_start=int(meetup_start.strftime('%s'))*1000
-    meetup_epoch_end=int(meetup_end.strftime('%s'))*1000
+    meetup_epoch_start = int(meetup_start.strftime('%s')) * 1000
+    meetup_epoch_end = int(meetup_end.strftime('%s')) * 1000
     for interest in interests:
         meetup_params = {
             'key': meetup_key,
@@ -280,7 +280,7 @@ def meetup_api(request):
             # Converts all times into strings and datetime objects from Unix time.
             # Also adds a timezone to datetime objects
             epoch_time = event['time']
-            start_dateTime_obj = datetime.datetime.fromtimestamp(epoch_time/1000)
+            start_dateTime_obj = datetime.datetime.fromtimestamp(epoch_time / 1000)
             start_dateTime = tz.localize(start_dateTime_obj)
             start_time = start_dateTime.strftime('%Y-%m-%dT%H:%M:%S-08:00')
 
@@ -288,12 +288,12 @@ def meetup_api(request):
             #Checks if returned event has a duration
             if event.get('duration'):
                 end_time_epoch = event['time'] + event['duration']
-                end_dateTime_obj = datetime.datetime.fromtimestamp(end_time_epoch/1000)
+                end_dateTime_obj = datetime.datetime.fromtimestamp(end_time_epoch / 1000)
                 end_dateTime = tz.localize(end_dateTime_obj)
-                end_time=end_dateTime.strftime('%Y-%m-%dT%H:%M:%S-08:00')
+                end_time = end_dateTime.strftime('%Y-%m-%dT%H:%M:%S-08:00')
             else:
-                end_dateTime = start_dateTime+relativedelta(hours=5)
-                end_time=end_dateTime.strftime('%Y-%m-%dT%H:%M:%S-08:00')
+                end_dateTime = start_dateTime + relativedelta(hours=5)
+                end_time = end_dateTime.strftime('%Y-%m-%dT%H:%M:%S-08:00')
 
             #Checks if event has venue and description
             if event.get('venue') and event.get('time') and event.get('name'):
@@ -314,12 +314,14 @@ def meetup_api(request):
                         user=request.user,
                         start_dateTime=start_dateTime_obj,
                         end_dateTime=end_dateTime_obj)}
-                )
+                    )
     success = {'success': 'success'}
     return HttpResponse(json.dumps(success), content_type="application/json")
 
 
 """Pulls biking, hiking, and trail related locations using Trail API"""
+
+
 @csrf_exempt
 def trail_api(request):
     profile = Profile.objects.get(user=request.user)
@@ -329,7 +331,9 @@ def trail_api(request):
     for interest in interests:
         activity = interest
         city = zipcode.city
-        url = ('https://outdoor-data-api.herokuapp.com/api.json?api_key='+outdoor_key+'&q[city_eq]={}&q[activities_activity_type_name_cont]={}&q[radius]=80'.format(city, activity))
+        url = (
+        'https://outdoor-data-api.herokuapp.com/api.json?api_key=' + outdoor_key + '&q[city_eq]={}&q[activities_activity_type_name_cont]={}&q[radius]=80'.format(
+            city, activity))
         resp = get(url=url)
         data = json.loads(resp.text)
         for outdoor in data['places']:
@@ -353,6 +357,8 @@ def bootstrap(request):
 
 """Matches events with users based if their free times match the event start dates and if those events fit the duration
 of their free times"""
+
+
 def matching(request):
     # Deletes any duplicate events from database for that specific user
     event_delete = Event.objects.filter(user=request.user)
@@ -376,7 +382,8 @@ def matching(request):
     events = Event.objects.filter(user=request.user).distinct()
 
     # Hash table with list of recommended events organized by category
-    matched_event = {'MUSIC':[], 'CAR': [], "TECHNOLOGY":[], "COMEDY": [], "HIKING": [], "BIKING":[], "TRAIL": [], "FOOD": [], "SPORTS": []}
+    matched_event = {'MUSIC': [], 'CAR': [], "TECHNOLOGY": [], "COMEDY": [], "HIKING": [], "BIKING": [], "TRAIL": [],
+                     "FOOD": [], "SPORTS": []}
     for free_time in free_times:
         for event in events:
             if event.start_dateTime and event.start_dateTime >= free_time.free_start_dateTime and free_time.free_end_dateTime:
@@ -388,14 +395,15 @@ def matching(request):
                 if event not in matched_event[event.category]:
                     matched_event[event.category].append(event)
 
-
     profile = Profile.objects.get(user=request.user)
     form = ProfileCreationForm()
 
-    return render(request, 'matched.html', {'matched': matched_event, 'timezone': profile.timezone, 'form':form })
+    return render(request, 'matched.html', {'matched': matched_event, 'timezone': profile.timezone, 'form': form})
 
 
 """Posts event to users google calendar"""
+
+
 @csrf_exempt
 def post_event(request):
     if request.method == 'POST':
@@ -403,8 +411,7 @@ def post_event(request):
         event = Event.objects.get(pk=event_id)
 
         # Parameters for event being saved to google calendar
-        print str(event.end_dateTime)+'.000-08.00', event.start_dateTime
-
+        print str(event.end_dateTime) + '.000-08.00', event.start_dateTime
 
         event_info = {
             "end": {
@@ -423,9 +430,9 @@ def post_event(request):
         # Builds google calendar service with appropriate credentials
         user_social_auth = request.user.social_auth.filter(provider='google-oauth2').first()
         access_token = user_social_auth.extra_data['access_token']
-        calID=user_social_auth.uid
+        calID = user_social_auth.uid
         credentials = AccessTokenCredentials(access_token, 'my-user-agent/1.0')
-        http= httplib2.Http()
+        http = httplib2.Http()
         http = credentials.authorize(http)
         service = build(serviceName='calendar', version='v3', http=http, developerKey=developer_key)
         created_event = service.events().insert(calendarId='primary', body=event_info).execute()
@@ -435,6 +442,7 @@ def post_event(request):
 
 def loading(request):
     return render(request, 'loading.html')
+
 
 # Finds events that match your friends recommended events
 def group_match(request):
@@ -460,6 +468,7 @@ def group_match(request):
     print matched
     return render(request, 'friend_match.html', {'matched': matched})
 
+
 @csrf_exempt
 def invite_friends(request):
     if request.method == "POST":
@@ -476,11 +485,13 @@ def invite_friends(request):
             message.add_to(friend)
             message.set_subject('You friend {} has invited you to BookIt'.format(request.user.first_name))
             message.set_text('Click the following link to get started now! http://localhost:8000/')
-            message.set_html("<h2>Welcome to BookIt</h2><p><a href='http://localhost:8000/'>Click Here To Get Started</a></p>")
+            message.set_html(
+                "<h2>Welcome to BookIt</h2><p><a href='http://localhost:8000/'>Click Here To Get Started</a></p>")
             message.set_from(request.user.email)
             status, msg = sg.send(message)
         success = {'success': 'success'}
         return HttpResponse(json.dumps(success), content_type="application/json")
+
 
 @csrf_exempt
 def add_friend(request):
@@ -524,28 +535,24 @@ def business_match(request):
     free_time_dict = {'timezone': profile.timezone}
     userdict = {}
     list_free_time = []
-
     free_time_delete = FreeTimes.objects.all()
-
-      # Deletes any events that have already happened
+    # Deletes any events that have already happened
     for row in free_time_delete:
         now = datetime.date.today()
-
         if free_time_delete.filter(free_start_dateTime__lte=now):
             row.delete()
         else:
             pass
-
     for free_time in business_free_times:
         bus_free_start = free_time.free_start_dateTime
         bus_free_end = free_time.free_end_dateTime
-        users_free_times = FreeTimes.objects.filter(free_start_dateTime__gte=bus_free_start).filter(free_end_dateTime__lte=bus_free_end)
+        users_free_times = FreeTimes.objects.filter(free_start_dateTime__gte=bus_free_start).filter(
+            free_end_dateTime__lte=bus_free_end)
         for user2 in users_free_times:
             # if user2 not in userdict[user2.user]:
             list_free_time.append(user2)
             userdict[user2.user] = list_free_time
     free_time_dict['info'] = userdict
-
     return render(request, 'business_match.html', free_time_dict)
 
 
